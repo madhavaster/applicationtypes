7bdff6630e63a0a9ad846e6ae419fa9d0b919782
Madhava Reddy <madhava@agrisoft.com>
2021-07-01 23:33:05 +0530

    initial commit

---
+++ src/main/java/com/agrisutra/ApplicationtypesApplication.java
@@ -0,0 +1,13 @@
+package com.agrisutra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationtypesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicationtypesApplication.class, args);
+	}
+
+}
